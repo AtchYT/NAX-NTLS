15c73d5e88f9efaa2a9525c10860ce8ee1658535
iNotAtch <131803570+AtchYT@users.noreply.github.com>
2025-04-29 14:55:37 +0200

    Updated

--- ntls_install.sh
+++ ntls_install.sh
@@ -3,6 +3,9 @@ echo "NAX Entertainment - v1.0.0 | Network Test and Log System"
 echo "Starting download of required packages"
 echo "Installing Termux-API"
 pkg install termux-api > /dev/null 2>&1
+echo "Installing Termux-API Battery and Telephone Status"
+pkg install termux-telephony-deviceinfo > /dev/null 2>&1
+pkg install termux-battery-status > /dev/null 2>&1
 echo "Installing Python3"
 pkg install python3 > /dev/null 2>&1
 echo "Installing Python3 PIP"
