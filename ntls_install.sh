3e1d4c5e2c5dc9127b0949c097d275b21a592119
iNotAtch <131803570+AtchYT@users.noreply.github.com>
2025-04-29 14:23:09 +0200

    Updated

--- ntls_install.sh
+++ ntls_install.sh
@@ -7,5 +7,9 @@ echo "Installing Python3"
 pkg install python3 > /dev/null 2>&1
 echo "Installing Python3 PIP"
 pkg install python3-pip > /dev/null 2>&1
+echo "Installing Python dependencies (pyfiglet and requests)"
+pip install pyfiglet requests
+echo "Installing Curl"
+pkg install curl > /dev/null 2>&1
 echo "Starting NAX-NTLS | v1.0.0"
 python3 ntls.py
