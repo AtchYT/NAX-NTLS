8cf4433f79cd81aaca27d4da771e75af8a125ca2
iNotAtch <131803570+AtchYT@users.noreply.github.com>
2025-04-29 13:49:14 +0200

    Updated

---
+++ ntls.sh
@@ -0,0 +1,19 @@
+clear
+echo "NAX Entertainment - v1.0.0 | Network Test and Log System\n\n"
+echo "Updating termux packages"
+pkg update > /dev/null 2>&1
+echo "Starting download of required packages\n"
+echo "Installing git"
+pkg install git > /dev/null 2>&1
+echo "Installing Termux-API"
+pkg install termux-api > /dev/null 2>&1
+echo "Installing Python3"
+pkg install python3 > /dev/null 2>&1
+echo "Installing Python3 PIP"
+pkg install python3-pip > /dev/null 2>&1
+echo "Downloading NAX-NTLS | v1.0.0"
+git clone https://github.com/AtchYT/NAX-NTLS.git > /dev/null 2>&1
+echo "Switching to NTLS directory"
+cd NAX-NTLS > /dev/null 2>&1
+echo "Starting NAX-NTLS | v1.0.0"
+python3 ntls.py
